01d1844c70919c042f9b93ed3adf7475d74885d8
shiveshrana <shiveshran8@gmail.com>
2026-09-11 20:57:26 +0530

    feat: configure secure S3 Terraform backend

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket       = "policy-as-code-terraform-state-shivesh-2026"
+    key          = "terraform.tfstate"
+    region       = "ap-south-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
