93e4b039b758155cf92600f18b4e7de9f0dab307
shiveshrana <shiveshran8@gmail.com>
2026-09-13 21:55:44 +0530

    test: introduce insecure s3 public policy

--- terraform/modules/infrastructure/main.tf
+++ terraform/modules/infrastructure/main.tf
@@ -90,7 +90,7 @@ resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
 
   block_public_acls       = true
-  block_public_policy     = true
+  block_public_policy     = false
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
